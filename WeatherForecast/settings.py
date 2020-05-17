738d6a019197d703b16e2baa0666e5c69607329c
Soban Farhan <soban.farhan@dcmail.ca>
2020-05-17 00:02:54 -0400

    Database
    Removed sql-lite database ( there was no use for it)

--- WeatherForecast/settings.py
+++ WeatherForecast/settings.py
@@ -75,13 +75,6 @@ WSGI_APPLICATION = 'WeatherForecast.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -100,7 +93,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
